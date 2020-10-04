e2b5474e780155512612f3d317876020e4cd6525
Omar <oemyoem55@gmail.com>
2020-10-04 14:21:51 +0500

    Static Exported

---
+++ build/precache-manifest.57908e779e2162c139cbd81706b399e0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5730340d3d8584bc638f11d271a7aa48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7d64eb9e658accbc2c9",
+    "url": "/static/js/2.1f474fdb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1f474fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7baed782202def49f1fd",
+    "url": "/static/js/main.b348c358.chunk.js"
+  },
+  {
+    "revision": "5bafd9575515df4d33b4",
+    "url": "/static/js/runtime-main.b991244d.js"
+  },
+  {
+    "revision": "4bba4fca7620bb4e13ff2cccb4f0058d",
+    "url": "/static/media/producBuoy.4bba4fca.svg"
+  },
+  {
+    "revision": "3a1df6c1e0644b8b1a12219346c3f013",
+    "url": "/static/media/productCurvyLines.3a1df6c1.png"
+  },
+  {
+    "revision": "a0b2ccce334edbf4107e46c0aef94b4a",
+    "url": "/static/media/productHowItWorks1.a0b2ccce.svg"
+  },
+  {
+    "revision": "138c5412d63f4ec5feac4282df15ba82",
+    "url": "/static/media/productHowItWorks2.138c5412.svg"
+  },
+  {
+    "revision": "8ca21716149eb07cd0557215642c47ce",
+    "url": "/static/media/productHowItWorks3.8ca21716.svg"
+  },
+  {
+    "revision": "5317c43c71e1f6199e6c0b297368091f",
+    "url": "/static/media/productValues1.5317c43c.svg"
+  },
+  {
+    "revision": "b8e7bdd2c72a7e5b0ce21b006e74f580",
+    "url": "/static/media/productValues2.b8e7bdd2.svg"
+  },
+  {
+    "revision": "40b631264f43f8c71f9a7801da84890b",
+    "url": "/static/media/productValues3.40b63126.svg"
+  }
+]);
